0f61e54b61b2a09c6376836e6f1d9fbd9c002420
Pedro Duarte <170323017@estudantes.ips.pt>
2019-11-14 21:57:17 +0000

    Inicial Commit

---
+++ Project.API/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Project.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Project.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+
+            return Ok(value);
+        }
+    }
+}
+
+
